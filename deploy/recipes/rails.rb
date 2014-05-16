314065141021ebd1a254547e2f6c973212511a73
Thiago <thiago.anunciacao@me.com>
2014-05-16 10:13:54 -0300

    update rails cookbook

--- deploy/recipes/rails.rb
+++ deploy/recipes/rails.rb
@@ -23,26 +23,34 @@ node[:deploy].each do |application, deploy|
     app application
   end
 
-  options = {
-    app: application,
-    user: deploy[:user],
-    log: "#{deploy[:deploy_to]}/shared/log",
-    # procfile: "#{deploy[:deploy_to]}/current/config/foreman/Procfile",
-    root: deploy[:current_path]
-  }.map { |k,v| "--#{k} #{v}" }.join(" ")
+  # options = {
+  #   app: application,
+  #   user: deploy[:user],
+  #   log: "#{deploy[:deploy_to]}/shared/log",
+  #   # procfile: "#{deploy[:deploy_to]}/current/config/foreman/Procfile",
+  #   root: deploy[:current_path]
+  # }.map { |k,v| "--#{k} #{v}" }.join(" ")
 
-  comando1 = "sudo bundle exec foreman export upstart /etc/init #{options}"
-  comando2 = "sudo /sbin/start #{application} || sudo /sbin/restart #{application}"
+  # comando1 = "sudo bundle exec foreman export upstart /etc/init #{options}"
+  # comando2 = "sudo /sbin/start #{application} || sudo /sbin/restart #{application}"
 
-  execute "export foreman config for #{application}" do
+  comando3 = "sudo bundle exec sidekiq -C config/sidekiq.yml"
+
+  execute "start sidekiq #{application}" do
     cwd deploy[:current_path]
-    command comando1
+    command comando3
     user deploy[:user]
   end
 
-  execute "start / restart foreman #{application}" do
-    cwd deploy[:current_path]
-    command comando2
-    user deploy[:user]
-  end
+  # execute "export foreman config for #{application}" do
+  #   cwd deploy[:current_path]
+  #   command comando1
+  #   user deploy[:user]
+  # end
+
+  # execute "start / restart foreman #{application}" do
+  #   cwd deploy[:current_path]
+  #   command comando2
+  #   user deploy[:user]
+  # end
 end
